14f00af20407c646665c41e5c55b5aa78a03a407
FABIAN <fabian.riess@live.de>
2024-06-13 15:16:54 +0200

    add navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import { FileText } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/entries">Entries</Link>
-        </li>
-        <li>
-          <Link href="/clientlist">Clients</Link>
-        </li>
-      </ul>
+      <Link href="/">Home</Link>
+      <Link href="/entries">
+        <FileText />
+      </Link>
+      <Link href="/clientlist">Clients</Link>
     </nav>
   );
 }
