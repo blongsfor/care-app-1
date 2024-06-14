ce0d0ac4817bb97f558991c363a80b14ebea862f
FABIAN <fabian.riess@live.de>
2024-06-14 11:57:02 +0200

    add dashboard functionality and uupdate navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,59 @@
 import { getSession } from "next-auth/react";
 import Navbar from "../components/Navbar";
 import LogoutButton from "../components/LogoutButton";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+const Dashboard = () => {
+  const router = useRouter();
+  const handleNavigation = (page) => {
+    router.push(page);
+  };
+
+  return (
+    <>
+      <div onClick={() => handleNavigation("/notes")}>
+        <Image
+          src="/square-check-big.svg"
+          alt="To-Do list icon"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div onClick={() => handleNavigation("/clientlist")}>
+        <Image
+          src="/users-round.svg"
+          alt="Clientlist icon"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div onClick={() => handleNavigation("/entries")}>
+        <Image
+          src="/file-text.svg"
+          alt="Entries icon"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div onClick={() => handleNavigation("/documentation-form")}>
+        <Image
+          src="/pencil-line.svg"
+          alt="Documentation Form icon"
+          width={100}
+          height={100}
+        />
+      </div>
+    </>
+  );
+};
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <LogoutButton />
       <h2>DASHBOARD</h2>
+      <Dashboard />
+      <LogoutButton />
     </>
   );
 }
